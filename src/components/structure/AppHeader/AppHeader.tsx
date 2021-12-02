c248a0a8bd97bf3248e3814331ff226d07064fa3
DanFullDev <justbe9523@gmail.com>
2021-12-02 09:24:26 -0500

    Logo change

--- src/components/structure/AppHeader/AppHeader.tsx
+++ src/components/structure/AppHeader/AppHeader.tsx
@@ -3,8 +3,8 @@ import TaskButton from "../../../common/TaskButton/TaskButton";
 
 export default function AppHeader() {
   return (
-    <div className="flex flex-row justify-between align-middle text-6xl text-white">
-      <div>TODO</div>
+    <div className="flex flex-row justify-between align-middle text-6xl text-white font-bold">
+      <div>T O D O</div>
       <TaskButton></TaskButton>
     </div>
   );
