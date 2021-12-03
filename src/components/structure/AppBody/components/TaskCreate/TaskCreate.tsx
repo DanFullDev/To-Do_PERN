ddc1ba2d8b0548fb9f900289e202fb9dcb12d3e9
DanFullDev <justeb9523@gmail.com>
2021-12-02 21:47:07 -0500

    Basic functionality skeleton for task create component finished

--- src/components/structure/AppBody/components/TaskCreate/TaskCreate.tsx
+++ src/components/structure/AppBody/components/TaskCreate/TaskCreate.tsx
@@ -1,6 +1,17 @@
 import "./TaskCreate.css";
 
 export default function TaskCreate() {
+  let todoValue: string | any;
+
+  const changeTodoValue = (event: any) => {
+    //Will call API to save this task and add it to the current task list
+    console.log(event);
+  };
+
+  const changeOnInput = (event: any) => {
+    todoValue = event.target.value;
+  };
+
   return (
     <div
       className="bg-gray-800 w-full h-24 mt-5 mb-5
@@ -9,13 +20,17 @@ export default function TaskCreate() {
     text-white justify-center"
     >
       <button
-        className="border-solid border-gray-700 border-2 
-        rounded-full h-10 w-10 mr-2 self-center"
+        className="border-solid border-gray-700 border-2
+        flex items-center justify-center
+        rounded-full h-10 w-10 mr-2 self-center transform duration-500
+        ease-in-out hover:scale-110 hover:text-blue-700"
         title="Create new todo"
+        onClick={($event) => changeTodoValue($event)}
       >
         +
       </button>
       <input
+        onInput={(value: any) => changeOnInput(value)}
         placeholder="Create a new todo..."
         className="bg-transparent text-xl flex-grow p-3 placeholder-white placeholder-opacity-20"
       ></input>
--- src/components/structure/AppFooter/AppFooter.tsx
+++ src/components/structure/AppFooter/AppFooter.tsx
@@ -2,7 +2,11 @@ import "./AppFooter.css";
 
 export default function AppFooter() {
   return (
-    <div className="font-semibold text-center text-gray-500 mt-5">
+    <div
+      className="font-semibold text-center text-gray-500 mt-5 
+      text-opacity-40 text-xl transform duration-500
+      ease-in-out hover:scale-110 cursor-pointer"
+    >
       Drag and drop to sort list
     </div>
   );
