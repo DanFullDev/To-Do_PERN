a836cd087c445ff91e2e73d3bf576e12f4009876
DanFullDev <justeb9523@gmail.com>
2021-12-02 20:53:00 -0500

    Initial create task bar

--- src/components/structure/AppBody/components/TaskCreate/TaskCreate.tsx
+++ src/components/structure/AppBody/components/TaskCreate/TaskCreate.tsx
@@ -2,6 +2,23 @@ import "./TaskCreate.css";
 
 export default function TaskCreate() {
   return (
-    <div className="bg-gray-800 w-full h-24 mt-5 mb-5 rounded-md cursor-pointer shadow-lg"></div>
+    <div
+      className="bg-gray-800 w-full h-24 mt-5 mb-5
+      rounded-md cursor-pointer shadow-lg p-5
+      flex flex-row align-middle
+    text-white justify-center"
+    >
+      <button
+        className="border-solid border-gray-700 border-2 
+        rounded-full h-10 w-10 mr-2 self-center"
+        title="Create new todo"
+      >
+        +
+      </button>
+      <input
+        placeholder="Create a new todo..."
+        className="bg-transparent text-xl flex-grow p-3 placeholder-white placeholder-opacity-20"
+      ></input>
+    </div>
   );
 }
--- src/components/structure/AppFooter/AppFooter.tsx
+++ src/components/structure/AppFooter/AppFooter.tsx
@@ -3,7 +3,7 @@ import "./AppFooter.css";
 export default function AppFooter() {
   return (
     <div className="font-semibold text-center text-gray-500 mt-5">
-      Made with the PERN stack. Tailwindcss.
+      Drag and drop to sort list
     </div>
   );
 }
