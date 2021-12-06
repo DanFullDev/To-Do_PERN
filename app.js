6187e5c82df08147e44d343069cda2d73355a30b
DanFullDev <justeb9523@gmail.com>
2021-12-06 18:49:09 -0500

    Changes for static file serving from client folder

--- app.js
+++ app.js
@@ -3,29 +3,22 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-var cors = require("cors");
 
 var todoRouter = require("./routes/todo");
 
 var app = express();
 
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.use(logger("dev"));
 app.use(express.json());
-app.use(cors);
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "client/build")));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-}
-
-//Routes
 app.use("/todo", todoRouter);
-
-//Sendback
-app.use("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "client/build", "index.html"))
-);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
