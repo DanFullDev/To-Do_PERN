5cd0a1846cbb37669a70c11ab4ea6888b8bb11e6
DanFullDev <justeb9523@gmail.com>
2021-12-06 19:15:22 -0500

    Finished basic queries

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "test1",
-  password: "12345",
+  user: "postgres",
+  password: "",
   host: "localhost",
   port: 5432,
   database: "perntodo",
--- models/todo.js
+++ models/todo.js
@@ -1,11 +1,27 @@
 const pool = require("../db");
 
 const Todo = {
-  async loadAll() {},
-  async loadOne(id) {},
-  async editOne(description, status, id) {},
-  async createOne(description, status) {},
-  async deleteOne(id) {},
+  async loadAll() {
+    return await pool.query("SELECT * FROM todo");
+  },
+  async loadOne(id) {
+    return await pool.query("SELECT * FROM todo WHERE todo_id = $1  ", [id]);
+  },
+  async editOne(description, status, id) {
+    return await pool.query(
+      "UPDATE todo SET description = $1, status = $2 WHERE todo_id = $3",
+      [description, status, id]
+    );
+  },
+  async createOne(description, status) {
+    return await pool.query(
+      "INSERT INTO todo (description, status) VALUES($1, $2)",
+      [description, status]
+    );
+  },
+  async deleteOne(id) {
+    return await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+  },
 };
 
 module.exports = Todo;
