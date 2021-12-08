c292b567af8743ceb4ba41d7deb28d901a8c32e8
DanFullDev <justeb9523@gmail.com>
2021-12-08 01:01:25 -0500

    change

--- app.js
+++ app.js
@@ -19,16 +19,16 @@ app.use(cookieParser());
 
 console.log(__dirname);
 
-app.use(express.static(path.join("..", "client", "build")));
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join("..", "client", "build")));
+  app.use(express.static(path.join(__dirname, "..", "client", "build")));
 }
 
 app.use("/todo", todoRouter);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join("..", "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
 });
 
 // catch 404 and forward to error handler
