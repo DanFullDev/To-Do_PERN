edbbf57033e6819b55cd414c9313db0722c3f0d4
DanFullDev <justeb9523@gmail.com>
2021-12-08 11:54:45 -0500

    Small style redesing

--- client/src/components/AppContainer.tsx
+++ client/src/components/AppContainer.tsx
@@ -6,7 +6,7 @@ import AppHeader from "./structure/AppHeader/AppHeader";
 export default function AppContainer() {
   return (
     <div className="h-80 bg-gradient-to-r from-blue-900 to-pink-900">
-      <div className="absolute z-50 ml-auto mr-auto left-0 right-0 text-center p-4 pt-1 pb-0 sm:p-20 sm:pt-12 sm:pb-0 xl:p-96 xl:pt-12 xl:pb-0">
+      <div className="absolute z-50 ml-auto mr-auto left-0 right-0 text-center pt-1 pb-0 sm:p-20 sm:pt-12 sm:pb-0 xl:p-96 xl:pt-12 xl:pb-0">
         <div className="flex flex-col justify-between align-middle">
           <AppHeader></AppHeader>
           <TaskList></TaskList>
