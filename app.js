75020989ed37724f423ae04e59b8806d993e3a6e
DanFullDev <justeb9523@gmail.com>
2021-12-08 00:35:03 -0500

    redirection error

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(express.static(path.join(__dirname, "client/build")));
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 }
